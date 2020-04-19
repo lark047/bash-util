5aebe4a3e7985eefa5c9099514c1680b4546db38
Andrew Clark <lark047@gmail.com>
2020-04-18 21:54:37 -0400

    added todo: simple to-do list

---
+++ todo.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+todo()
+{
+    case ${1} in
+        add)
+            shift
+            d="$(date +"%F %T")"
+            [[ -r ~/.todo ]] || touch ~/.todo
+            lines=$(wc -l ~/.todo | awk '{print $1}')
+            printf "%d: %s $*\n" "$(( lines + 1 ))" "${d}" >> ~/.todo
+            printf "TODO: Added \"$*\" [%s]\n" "${d}"
+            ;;
+
+        mark)
+            shift
+            if grep "$*" ~/.todo; then
+                read -rp "Mark these notes completed? [Y/N] " response
+                case ${response,,} in
+                    yes|y)
+                        sed -i "/$*/s/^/COMPLETED /" ~/.todo
+                        ;;
+                esac
+            fi
+            ;;
+
+        list)
+            shift
+            printf '\nTODO list:\n'
+            pattern="${1}"
+            [ -n "${pattern}" ] || pattern=___
+            shift
+            case "${pattern}" in
+                all)
+                    grep --color=never "${*:-.*}" ~/.todo
+                    ;;
+                ___)
+                    grep -v "^COMPLETED " ~/.todo | grep --color=never "${1:-.*}"
+                    ;;
+                *)
+                    grep --color=never "${pattern}" ~/.todo
+                    ;;
+            esac
+            LAST_TODO_LIST=$(date +%s)
+            export LAST_TODO_LIST
+            ;;
+
+        *)
+            printf 'Usage: todo [add|mark|list]\n'
+            printf '    add \tadds a todo\n'
+            printf '    mark\tmarks a todo done\n'
+            printf '    list\tlists incomplete todos\n'
+            return 0
+            ;;
+
+    esac
+}
